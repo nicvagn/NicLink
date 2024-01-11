0543f720c8b8f73ce83e641ec4c0f49aacd8a268
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-01-11 15:06:41 -0600

    Cleaning up for a merge back into master

--- src/NicLink.cpp
+++ src/NicLink.cpp
@@ -1,11 +1,11 @@
 #include <python3.12/Python.h>
 #include <pybind11/pybind11.h>
-//#include <pybind11/iostream.h>
+#include <pybind11/iostream.h>
 #include "EasyLink.h"
 #include <string>
 #include <iostream>
 
-//namespace py = pybind11;
+namespace py = pybind11;
 
 //the link to the board
 shared_ptr<ChessLink> chessLink = nullptr;
@@ -23,12 +23,11 @@ int add(int i, int j)
 //set up connection, and set up real time callback
 void connect()
 {
-
     chessLink = ChessLink::fromHidConnect();
 
     if( !(chessLink -> switchUploadMode()) )
     {
-        cout << "ERROR: CAN NOT SWITCH TO UPLOAD MODE." << endl;
+        cerr << "ERROR: CAN NOT SWITCH TO UPLOAD MODE." << endl;
     }
 
     chessLink -> connect();
@@ -38,27 +37,32 @@ void connect()
     //will be true on a sucess else false
     if( chessLink -> switchUploadMode() )
     {
-        printf("Switch upload mode a success");
+        cout << "Switch upload mode a success" << endl;
         chessLink -> setRealTimeCallback(
             [](string s) {
                 //keep the current fen up to date
                 currentFen = s;
-                cout << s;
             });
     }
     else
     {
-        printf("ERROR: CAN NOT SWITCH TO OUTPUT MODE.");
+        cerr << "ERROR: CAN NOT SWITCH TO OUTPUT MODE." << endl;
     }
 
     chessLink -> switchRealTimeMode();
+    this_thread::sleep_for(chrono::seconds(2));
+
     cout << "connect out, chessboard in realtime mode" << endl;
 }
 
 //disconnect
-void disconect()
+void disconnect()
 {
-    
+    if(chessLink == nullptr)
+    {
+        cerr << "chesslink is not connected." << endl;
+        return;
+    }
 
     //turn off all the lights
     chessLink -> setLed({
@@ -76,83 +80,6 @@ void disconect()
     chessLink -> disconnect();    
 }
 
-void testChess() {
-  {
-    printf("test chess entered \n");
-
-    connect();
-   
-    chessLink->beep();
-
-    chessLink->switchRealTimeMode();
-
-    // cout << ch->getMcuVersion() << endl;
-
-    // cout << ch->getBleVersion() << endl;
-
-    cout << chessLink ->getBattery() << endl;
-
-    // cout << ch->getFileCount() << endl;
-
-    /*
-    while (true) {
-      auto s = ch->getFile(true);
-
-      cout << s.size() << endl;
-
-      if (s.size() == 0) {
-        return;
-      }
-    }
-    */
-
-
-    bitset<8> ll[8] = {
-        bitset<8>("10000000"), //
-        bitset<8>("01000000"), //
-        bitset<8>("00100000"), //
-        bitset<8>("00010000"), //
-        bitset<8>("00001000"), //
-        bitset<8>("00000100"), //
-        bitset<8>("00000010"), //
-        bitset<8>("00000001"), //
-    };
-
-    int i = 0;
-    while(i < 10) {
-      chessLink->setLed({
-          ll[i % 8],
-          ll[(i + 1) % 8],
-          ll[(i + 2) % 8],
-          ll[(i + 3) % 8],
-          ll[(i + 4) % 8],
-          ll[(i + 5) % 8],
-          ll[(i + 6) % 8],
-          ll[(i + 7) % 8],
-      });
-      this_thread::sleep_for(chrono::seconds(1));
-      cout << currentFen;
-      i++;
-    }
-
-    chessLink->disconnect();
-  }
-
-  // unsigned char buf[] = {0x21, 0x01, 0x00};
-  // auto r = ch.write(buf, sizeof(buf));
-
-  // while (true) {
-  //   unsigned char readBuf[65] = {0};
-  //   auto l = ch.read(readBuf, 65);
-  //   for (int i = 0; i < sizeof(readBuf); i++) {
-  //     cout << hex << " " << int(readBuf[i]);
-  //   }
-  //   cout << endl;
-  //   cout << l << endl;
-  // }
-
-  // cout << ch->switchRealTimeMode() << endl;
-}
 
 //get the current fen of the board
 string getFEN()
@@ -160,7 +87,7 @@ string getFEN()
     //if we have not connected throw error and return
     if( chessLink == nullptr )
     {
-        fprintf(stderr, "bChessLink is nullptr. Are you sure you are connected to board?");
+        cerr << "bChessLink is nullptr. Are you sure you are connected to board?" << endl;
         return "ERROR: no connection.";
     }
         
@@ -168,11 +95,37 @@ string getFEN()
     return currentFen;
 }
 
+//set an led on the chessboard
+void setLED(int x, int y, bool LEDsetting)
+{
+    //if we have not connected throw error and return
+    if( chessLink == nullptr )
+    {
+        cerr << "bChessLink is nullptr. Are you sure you are connected to board?" << endl;
+        return;
+    }
+
+    if(x > 7 || x < 0)
+    {
+        cerr << "x must be 0 - 7, x is: " << x << endl;
+        return;
+    }
+    if(y > 7 || y < 0)
+    {
+        cerr << "y must be 0 - 7, y is: " << y << endl;
+        return;
+    }
+
+    chessLink -> setLed((uint8_t) x, (uint8_t) y, LEDsetting);
+}
+
+
 
 int main()
 {
     /* connect to the board */
     connect();
+    chessLink -> ChessLink::setLed((uint8_t) 4, (uint8_t) 4, true);
 }
 
 
@@ -185,16 +138,28 @@ PYBIND11_MODULE(NicLink, m)
     // connect with a redirected out to py
     m.def("connect", []() { 
         py::scoped_ostream_redirect stream(
-            std::cout,                                // std::ostream&
+            std::cerr,                                // std::ostream&
             py::module_::import("sys").attr("stdout") //python output
         );
         connect();
     }, "connect to chess board device with hid even if the device is not connected,\nit will automatically connect when the device is plugged into the computer");
     
-    m.def("disconnect", &disconect, "Disconect from the chessboard.");
+    m.def("disconnect", []() {
+            py::scoped_ostream_redirect stream(
+            std::cerr,                                // std::ostream&
+            py::module_::import("sys").attr("stdout") //python output
+        );
+        disconnect();
+    }, "disconnect from the chessboard.");
 
+    // switch modes
     m.def("uploadMode", &ChessLink::switchUploadMode, "Switch to upload mode.");
     m.def("realTimeMode", &ChessLink::switchRealTimeMode, "Switch to realtime mode.");
 
-    m.def("testChess", &testChess, "test chess");
+    // seters
+    m.def("setLED", &setLED, "Set a LED on the chessboard.");
+    // getters
+    m.def("getFEN", &getFEN, "Get the FEN for the chessboard's cur position.");
 }
+
+
