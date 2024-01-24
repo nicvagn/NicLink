742ca525d8a7b2420ba09037f8584b3f523e79ff
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-01-24 08:31:17 -0600

    well on the way to a complete PIP module.

--- nic_soft/niclink/niclink.py
+++ nic_soft/niclink/niclink.py
@@ -4,64 +4,66 @@
 #  NicLink is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License along with NicLink. If not, see <https://www.gnu.org/licenses/>. 
-
-from _niclink import connect, disconect, uploadMode, realTimeMode, getFEN, setLED, beep
+import _niclink
+from _niclink import connect, disconnect, uploadMode, realTimeMode, getFEN, setLED, beep
 import time
 import chess
+import copy
+import readchar
 
-
-
-refresh_delay = 4 #The time between checks of board for move
-
-game_board = chess.Board()
 
 class NicLink:
     """ manage Chessnut air external board """
-
+    
     def __init__( self, refresh_delay ):
         """ initialize the link to the chessboard, and set up NicLink """
         self.refresh_delay = refresh_delay
         # initialize the chessboard, this must be done first, before chattering at it
-        connect()
+        self.connect()
+        # this instances game board
+        self.game_board = chess.Board()
 
-    def connect():
+    def connect( self ):
         """ connect to the chessboard """
         # connect with the external board
-        _niclink.cconnect()
+        _niclink.connect()
         # make sure getFEN is working
         print(f"initial fen: { _niclink.getFEN() }")
         print("Board initialized")
 
 
-    def beep():
+    def beep( self ):
         """ make the chessboard beep """
-        _niclink.cbeep()
+        _niclink.beep()
+
+    def getFEN():
+        """ get the FEN from chessboard """
+        return _niclink.getFEN();
 
 
-    def find_move_from_fen_change( new_FEN ):
+    def find_move_from_fen_change( self, new_FEN ):
         """ get the move that occured to change the game_board fen into a given FEN """
-        global game_board
 
         # get a list of the legal moves
-        legal_moves = list(game_board.legal_moves)
-
-        print(f"board now: \n{ game_board }")
-        print(f"board we are using to check legal moves: \n{game_board}")
+        legal_moves = list(self.game_board.legal_moves)
+        
+        print(f"board now: \n{ self.game_board }")
+        tmp_board = self.game_board.copy()
+        print(f"board we are using to check legal moves: \n{self.game_board}")
 
         for move in legal_moves:
             #print(move)
             #breakpoint()
-            game_board.push(move)  # Make the move on the board
-            if game_board.board_fen() == new_FEN:  # Check if the board's FEN matches the new FEN
+            tmp_board.push( move )  # Make the move on the board
+            if tmp_board.board_fen() == new_FEN:  # Check if the board's FEN matches the new FEN
                 return move  # Return the move in Coordinate notation 
-            game_board.pop()  # Undo the move
+            tmp_board.pop()  # Undo the move
 
         raise RuntimeError("a valid move was not made")
 
 
-    def check_for_move():
+    def check_for_move( self ):
         """ check if there has been a move on the chessboard, and see if it is valid """
-        global game_board
 
         # ensure the move was valid
 
@@ -73,61 +75,40 @@ class NicLink:
 
         new_FEN = tmpFEN
         
-        if( new_FEN != game_board.board_fen ):
+        if( new_FEN != self.game_board.board_fen ):
             # a change has occured on the chessboard
-            print("CHANGE")
-            print(f"board now: \n{game_board}\n")
+            print("move done")
+            print(f"board prior to move: \n{self.game_board}\n")
+            return True
 
-            #check if move is valid
-            move = find_move_from_fen_change( new_FEN )
-            if( move != None and move != ''):
-                # if it is a valid move, return true
-                print(f"{move} is it the move")
-                
-                return True
         else:
             print("no change")
         return False
 
 
-    def set_FEN_on_board( board, FEN ):
+    def set_FEN_on_board( self, board, FEN ):
         """ set a board up according to a FEN """
         board = chess.Board()
         chess.Board.set_board_fen( board, fen=FEN)
 
 
-    def show_FEN_on_board( FEN ):
+    def show_FEN_on_board( self, FEN ):
         """ print a FEN on on a chessboard """
         board = chess.Board()
-        set_FEN_on_board( board, FEN )
+        self.set_FEN_on_board( board, FEN )
         print( board )
 
 
 # if module is on "top level" ie: run directly
 if __name__ == '__main__':
-    
-    
-    niclink = NicLink()
-    # initial values for fen's
-    newFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+    nl_instance = NicLink( 2 )
 
-    while( True ):
-        #show_FEN_on_board( "rnbqkbnr/pppppppp/8/8/8/4P3/PPPP1PPP/RNBQKBNR")
-        try:
-            if( check_for_move() ):
-                print("change in board.")
-                print(f"new fen: \n {newFEN}\n")
+    leave = 'n' 
+    while( leave == 'n' ):
+        if( nl_instance.check_for_move() ):
+            print("move")            
+            
 
-                if(newFEN == '' or newFEN == None):
-                    print("pastFEN is None")
-                    pass
-                else:
-                    move = find_move_from_fen_change( new_FEN=newFEN )
-                    print(f"==== MOVE WAS: {move} ====")
+        print("leave? (n for no, != 'n' yes: ")
+        leave = readchar.readkey()
 
-        except (RuntimeError, ValueError) as err:
-            print(err)
-
-        # print(f"====== {currentFEN} ======")
-        time.sleep(refresh_delay)
-        NicLink.beep()
