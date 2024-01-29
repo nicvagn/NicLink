37fe24ea57d7370a9280042e04f071b1424038f5
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-01-28 20:36:14 -0600

    stole the lighess api stuff from CLI chess

---
+++ nic_soft/token_manager/token_manager_model.py
@@ -0,0 +1,125 @@
+# Copyright (C) 2021-2023 Trevor Bayless <trevorbayless1@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from cli_chess.utils.config import lichess_config
+from cli_chess.utils import Event, log, threaded
+from berserk import Client, TokenSession
+import berserk.exceptions
+
+linked_token_scopes = set()
+
+
+class TokenManagerModel:
+    def __init__(self):
+        self.base_url = ""
+        self.linked_account = ""
+        self.e_token_manager_model_updated = Event()
+
+    def set_base_url(self, url: str):
+        """Sets the base URL for all requests to go to"""
+        self.base_url = url
+        log.debug(f"Base URL set to: {self.base_url}")
+
+    @threaded
+    def validate_existing_linked_account(self) -> None:
+        """Queries the Lichess config file for an existing token. If a token
+           exists, verification is attempted. Invalid data will be cleared.
+        """
+        try:
+            existing_token = lichess_config.get_value(lichess_config.Keys.API_TOKEN)
+            account = self.validate_token(existing_token)
+            if account:
+                self.save_account_data(api_token=existing_token, account_data=account, valid=True)
+            else:
+                self.save_account_data(api_token="", account_data={})
+        except Exception as e:
+            # Rather than the token being invalid, this means there was a
+            # connection problem. Ignore so the existing token is not overridden.
+            if not isinstance(e, berserk.exceptions.ApiError):
+                log.error(f"Unexpected exception caught: {e}")
+
+    def update_linked_account(self, api_token: str) -> bool:
+        """Attempts to update the linked account using the passed in API token.
+           If the token is deemed valid, the api token is saved to the Lichess
+           configuration. Returns True on success. Existing account data is
+           only overwritten on success.
+         """
+        if api_token:
+            try:
+                account = self.validate_token(api_token)
+                if account:
+                    log.info("Updating linked Lichess account")
+                    self.save_account_data(api_token=api_token, account_data=account, valid=True)
+                    return True
+            except Exception as e:
+                log.error(f"Error updating linked account: {e}")
+                return False
+        return False
+
+    def validate_token(self, api_token: str) -> dict:
+        """Validates the proper scopes are available for the passed in token.
+           Returns the scopes and userId associated to the passed in token.
+        """
+        if api_token:
+            session = TokenSession(api_token)
+            oauth_client = Client(session, base_url=self.base_url).oauth
+            try:
+                token_data = oauth_client.test_tokens(api_token)
+
+                if token_data.get(api_token):
+                    found_scopes = set()
+                    for scope in token_data[api_token]['scopes'].split(sep=","):
+                        found_scopes.add(scope)
+
+                    from cli_chess.core.api.api_manager import required_token_scopes
+                    if found_scopes >= required_token_scopes:
+                        global linked_token_scopes
+                        linked_token_scopes.clear()
+                        linked_token_scopes = found_scopes
+
+                        log.info("Successfully authenticated with Lichess")
+                        return token_data[api_token]
+                    else:
+                        log.error("Valid token but missing required scopes")
+
+            except Exception as e:
+                log.error(f"Error validating token: {e}")
+                raise
+
+    def save_account_data(self, api_token: str, account_data: dict, valid=False) -> None:
+        """Saves the passed in lichess api token to the configuration.
+           It is assumed the passed in token has already been verified
+        """
+        lichess_config.set_value(lichess_config.Keys.API_TOKEN, api_token)
+        if valid:
+            self.linked_account = account_data.get('userId', "")
+            self._handle_start_api(api_token)
+
+        self._notify_token_manager_model_updated()
+
+    def _handle_start_api(self, api_token: str) -> None:
+        """Handles starting the API using the supplied token.
+           This function should only ever be called by this model.
+        """
+        if api_token.strip():
+            from .. api import _start_api  # noqa
+            _start_api(api_token, self.base_url)
+
+    def _notify_token_manager_model_updated(self) -> None:
+        """Notifies listeners of token manager model updates"""
+        self.e_token_manager_model_updated.notify()
+
+
+g_token_manager_model = TokenManagerModel()
---
+++ nic_soft/token_manager/token_manager_view.py
@@ -0,0 +1,104 @@
+# Copyright (C) 2021-2023 Trevor Bayless <trevorbayless1@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+from cli_chess.utils.ui_common import handle_mouse_click, handle_bound_key_pressed
+from prompt_toolkit.layout import Window, ConditionalContainer, VSplit, HSplit, D
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.formatted_text import StyleAndTextTuples
+from prompt_toolkit.widgets import Label, Box, TextArea, ValidationToolbar
+from prompt_toolkit.validation import Validator
+from prompt_toolkit.filters import Condition
+from prompt_toolkit.application import get_app
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from cli_chess.modules.token_manager import TokenManagerPresenter
+
+
+class TokenManagerView:
+    def __init__(self, presenter: TokenManagerPresenter):
+        self.presenter = presenter
+        self.container_width = 40
+        self.lichess_username = ""
+        self._token_input = self._create_token_input_area()
+        self._container = self._create_container()
+
+    def _create_token_input_area(self):
+        """Creates and returns the TextArea used for token input"""
+        validator = Validator.from_callable(
+            self.presenter.update_linked_account,
+            error_message="Invalid token or missing scopes",
+            move_cursor_to_end=True,
+        )
+
+        return TextArea(
+            validator=validator,
+            accept_handler=lambda x: True,
+            style="class:text-area.input",
+            focus_on_click=True,
+            wrap_lines=True,
+            multiline=False,
+            width=D(max=self.container_width),
+            height=D(max=1),
+        )
+
+    def _create_container(self) -> HSplit:
+        """Creates the container for the token manager view"""
+        return HSplit([
+            Label(f"{'Authenticate with Lichess':<{self.container_width}}", style="class:menu.category-title", wrap_lines=False),
+            VSplit([
+                Label("API Token: ", style="bold", dont_extend_width=True),
+                ConditionalContainer(
+                    TextArea("Input token and press enter", style="class:text-area.input.placeholder", focus_on_click=True),
+                    filter=Condition(lambda: not self.has_focus()) & Condition(lambda: len(self._token_input.text) == 0)
+                ),
+                ConditionalContainer(self._token_input, Condition(lambda: self.has_focus()) | Condition(lambda: len(self._token_input.text) > 0)),
+            ], height=D(max=1)),
+            ValidationToolbar(),
+            Box(Window(), height=D(max=1)),
+            VSplit([
+                Label("Linked account: ", dont_extend_width=True),
+                ConditionalContainer(Label("None", style="class:label.error bold italic"), Condition(lambda: not self.lichess_username)),
+                ConditionalContainer(Label(text=lambda: self.lichess_username, style="class:label.success bold"), Condition(lambda: self.lichess_username != "")),  # noqa: E501
+            ], height=D(max=1)),
+        ], width=D(max=self.container_width), height=D(preferred=8))
+
+    def get_function_bar_fragments(self) -> StyleAndTextTuples:
+        """Returns a set of function bar fragments to use if
+           this module is hooked up with a function bar
+        """
+        return [
+            ("class:function-bar.key", "F1", handle_mouse_click(self.presenter.open_token_creation_url)),
+            ("class:function-bar.label", f"{'Open token creation URL':<25}", handle_mouse_click(self.presenter.open_token_creation_url)),
+        ]
+
+    def get_function_bar_key_bindings(self) -> KeyBindings:
+        """Returns a set of key bindings to use if this
+           module is hooked up with a function bar
+        """
+        kb = KeyBindings()
+        kb.add(Keys.F1)(handle_bound_key_pressed(self.presenter.open_token_creation_url))
+        return kb
+
+    def has_focus(self):
+        """Returns true if this container has focus"""
+        has_focus = get_app().layout.has_focus(self._container)
+        if has_focus:
+            get_app().layout.focus(self._token_input)
+        return has_focus
+
+    def __pt_container__(self) -> HSplit:
+        return self._container
