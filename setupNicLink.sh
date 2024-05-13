20a29ac498cf6eb093374c54b3219155dfb0b7ed
Nicolas Vaagen <37600758+nicvagn@users.noreply.github.com>
2024-05-13 11:01:28 -0600

    ADDED: a test script for help install

---
+++ setupNicLink.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# make a special test_NicLink dir
+echo "WARN - will fetch python requirements, but Not C++ ones"
+
+mkdir -p ~/test_Niclink
+
+cd ~/test_Niclink/
+echo "Cloning NicLink into your Home folder."
+
+git clone https://github.com/nicvagn/NicLink 
+
+echo "cloning submodules"
+cd NicLink
+git submodule update --init --recursive .
+
+
+
+echo "making python virtual env"
+python3 -m venv venv 
+
+echo "entering the venv"
+. ~/test_Niclink/venv/bin/activate
+
+echo "ensuring the python package manager is installed"
+python -m ensurepip --upgrade
+
+
+cd ~/test_Niclink/NicLink
+
+python -m pip install -r requirements.txt
+
+echo "building NicLink"
+./updateNicLink.sh
+
