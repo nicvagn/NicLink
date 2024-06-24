418749d2d2c6ff1f4d138d791efca23144a07099
Nicolas Vaagen <37600758+nicvagn@users.noreply.github.com>
2024-06-24 14:00:48 -0600

    chore: fixup to help with installing new instance

--- setupNicLink.sh
+++ setupNicLink.sh
@@ -3,18 +3,12 @@
 echo "WARN - will fetch python requirements, but Not C++ ones"
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-cd $SCRIPT_DIR/test_Niclink
-
-echo "Cloning NicLink into your Home folder."
-
-git clone https://github.com/nicvagn/NicLink 
 
 echo "cloning submodules"
-cd NicLink
+cd SCRIPT_DIR
+
 git submodule update --init --recursive .
-
 git pull
-
 
 echo "making python virtual env"
 python3 -m venv venv 
@@ -25,7 +19,7 @@ echo "entering the venv"
 echo "ensuring the python package manager is installed"
 python -m ensurepip --upgrade
 
-echo "installing berserk from the github"
+echo "installing berserk from the github. The one from pip does not work currently"
 cd ${SCRIPT_DIR}
 git clone https://github.com/lichess-org/berserk.git/
 
