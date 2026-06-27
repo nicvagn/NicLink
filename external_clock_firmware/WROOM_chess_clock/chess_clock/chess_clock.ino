ccf308e81c206df2ef17ab15582025e54fe74072
Nicolas Vaagen <nicolasvaagen@gmail.com>
2026-06-27 14:22:20 -0600

    BIG re-due of chess clock firmware. Set to move with time

--- external_clock_firmware/WROOM_chess_clock/chess_clock/chess_clock.ino
+++ external_clock_firmware/WROOM_chess_clock/chess_clock/chess_clock.ino
@@ -1,8 +1,8 @@
-// file: /data/git/niclink/standalone_chessclock/external_clock_firmware/WROOM_chess_clock/chess_clock/chess_clock.ino
+// file: /data/git/niclink/external_clock_firmware/WROOM_chess_clock/chess_clock/chess_clock2.ino
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
-//#define DEBUG T
+#define DEBUG T
 //#define DEBUG_TICK T
 // messages
 #define WHITE_BLACK "|white|-|black| "
@@ -24,9 +24,10 @@
 // default times
 #define B_START_TIME 600000
 #define W_START_TIME 600000
-#define W_INCREMENT 6000
-#define B_INCREMENT 6000
+#define W_START_INC 6000
+#define B_START_INC 6000
 
+// button bins
 #define RESET_BTN_PIN 4
 #define MOVE_MADE_PIN 5
 
@@ -40,15 +41,12 @@
  */
 LiquidCrystal_I2C lcd(LCD_ADDR, LCD_ROWS, LCD_COL);
 
-enum Colour { white,
-              black };
-
 // Button structure to hold all state
 struct Button {
   uint8_t pin;
-  int curRead;
-  int lastState;
-  unsigned long lastDebounceTime;
+  int8_t curRead;
+  int8_t lastState;
+  uint32_t lastDebounceTime;
   const char *name;
 };
 
@@ -59,6 +57,7 @@ Button resetBtn = {
   0,
   "Reset Button",
 };
+
 Button moveBtn = {
   MOVE_MADE_PIN,
   HIGH,
@@ -68,125 +67,110 @@ Button moveBtn = {
 };
 
 
-uint32_t whiteTimeMs = W_START_TIME;
-uint32_t whiteIncMs = W_INCREMENT;
-uint32_t blackTimeMs = B_START_TIME;
-uint32_t blackIncMs = B_INCREMENT;
+// init times
+uint32_t whiteStartingTimeMs = B_START_TIME;
+uint32_t whiteStartingIncMs = W_START_INC;
+uint32_t blackStartingTimeMs = B_START_TIME;
+uint32_t blackStartingIncMs = B_START_INC;
+// current times
+uint32_t whiteTimeMs = whiteStartingTimeMs;
+uint32_t whiteIncMs = whiteStartingIncMs;
+uint32_t blackTimeMs = blackStartingTimeMs;
+uint32_t blackIncMs = blackStartingIncMs;
 
 bool connected = false;
 bool whiteToPlay = true;
-uint32_t lastUpdate = 0;
-bool gameOver = false;
 bool clockRunning = false;
+uint32_t lastUpdate = 0;
 
-void startClock() {
-  clockRunning = true;
-  gameOver = false;
+void setup() {
+  Wire.begin(SDA_PIN, SCL_PIN);
+  // start serial connection
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(10);
+  }
+
+  lcd.init();
+  lcd.backlight();
+  reset();
+  displayTime();
   lastUpdate = millis();
-  lcd.setCursor(0, 0);
-  lcd.print(WHITE_TURN);
 #ifdef DEBUG
-  Serial.println("CLOCK_STARTED");
+  Serial.println("CLOCK READY");
 #endif
+  // button pins
+  pinMode(moveBtn.pin, INPUT_PULLUP);
+  pinMode(resetBtn.pin, INPUT_PULLUP);
 }
 
-void secondsToHMS(uint32_t seconds, uint16_t &h, uint16_t &m, uint16_t &s) {
+// reset chess clock
+void reset() {
+  clockRunning = false;
+  whiteToPlay = true;
+  lastUpdate = 0;
+
+  whiteTimeMs = whiteStartingTimeMs;
+  whiteIncMs = whiteStartingIncMs;
+  blackTimeMs = blackStartingTimeMs;
+  blackIncMs = blackStartingIncMs;
+  lcd.setCursor(0, 0);
+  lcd.print(WHITE_BLACK);
+
+  Serial.println("CLOCK RESET");
+}
+
+void gameDone() {
+  clockRunning = false;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(GAME_OVER);
+  lcd.setCursor(0, 1);
+  lcd.print(GAME_OVER);
+  Serial.println("GAME_OVER");
+}
+
+
+void checkButton(Button &btn) {
+  int reading = digitalRead(btn.pin);
+  // If the reading changed, reset debounce timer
+  if (reading != btn.curRead) {
+    btn.lastDebounceTime = millis();
+    btn.curRead = reading;
+    // If enough time has passed, accept the reading as stable
+    if ((millis() - btn.lastDebounceTime) > DEBOUNCE_DELAY) {
+      // If the stable state has changed
+      if (btn.curRead != btn.lastState) {
+        btn.lastState = btn.curRead;
+        if (btn.curRead == LOW) {
+#ifdef DEBUG
+          Serial.print(btn.name);
+          Serial.println(" pressed");
+#endif
+          if (btn.pin == MOVE_MADE_PIN) {
+            Serial.println("TODO move ptn");
+            // TODO
+          } else if (btn.pin == RESET_BTN_PIN) {
+            reset();
+          }
+        }
+      }
+    }
+  }
+}
+
+
+void secondsToHMS(uint16_t seconds, uint16_t &h, uint16_t &m, uint16_t &s) {
   s = seconds % 60;
-  uint32_t minutes = seconds / 60;
+  uint16_t minutes = seconds / 60;
   m = minutes % 60;
   h = minutes / 60;
 }
 
-void blackMoved() {
-  clockRunning = true;
-  whiteToPlay = true;
-  lcd.setCursor(0, 0);
-  lcd.print(WHITE_TURN);
-  blackTimeMs = blackTimeMs + blackIncMs;
-}
-
-void whiteMoved() {
-  clockRunning = true;
-  whiteToPlay = false;
-  lcd.setCursor(0, 0);
-  lcd.print(BLACK_TURN);
-  whiteTimeMs = whiteTimeMs + whiteIncMs;
-}
-
-void whiteTimeOut() {
-  whiteTimeMs = 0;
-  gameOver = true;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(TIME_UP);
-  lcd.setCursor(0, 1);
-  lcd.print(BLACK_WIN);
-  Serial.println("Game Over: Black wins on time");
-}
-
-void blackTimeOut() {
-  whiteTimeMs = 0;
-  gameOver = true;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(TIME_UP);
-  lcd.setCursor(0, 1);
-  lcd.print(WHITE_WIN);
-  Serial.println("Game Over: White wins on time");
-}
-
-void whiteWon() {
-  gameOver = true;
-  clockRunning = false;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(GAME_OVER);
-  lcd.setCursor(0, 1);
-  lcd.print(WHITE_WIN);
-  Serial.println("Game over: White wins.");
-}
-
-void blackWon() {
-  gameOver = true;
-  clockRunning = false;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(GAME_OVER);
-  lcd.setCursor(0, 1);
-  lcd.print(BLACK_WIN);
-  Serial.println("Game over: Black wins.");
-}
-
-void draw() {
-  gameOver = true;
-  clockRunning = false;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(DRAW);
-  lcd.setCursor(0, 1);
-  lcd.print(DRAW);
-  Serial.println("Game over: Draw");
-}
-
-void gameDone() {
-  // if game is over, do not process
-  if (gameOver) {
-    return;
-  }
-  gameOver = true;
-  clockRunning = false;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(GAME_OVER);
-  lcd.setCursor(0, 1);
-  lcd.print(GAME_OVER);
-  Serial.println("GAME_OVER:UNKNOWN");
-}
-
 void displayTime() {
   // convert from ms to seconds
-  uint32_t wTotalSec = whiteTimeMs / 1000;
-  uint32_t bTotalSec = blackTimeMs / 1000;
+  uint16_t wTotalSec = whiteTimeMs / 1000;
+  uint16_t bTotalSec = blackTimeMs / 1000;
 
   // White and black hours, minutes and seconds
   uint16_t wH, bH;
@@ -233,41 +217,8 @@ void displayTime() {
   lcd.print("    ");
 }
 
-void moveMade() {
-  if (gameOver) {
-    gameOver = false;
-  }
-
-  if (!clockRunning) {
-    startClock();
-  }
-  if (whiteToPlay) {
-    whiteMoved();
-  } else {
-    blackMoved();
-  }
-}
-
-
-void reset() {
-  whiteTimeMs = W_START_TIME;
-  blackTimeMs = B_START_TIME;
-  whiteIncMs = W_INCREMENT;
-  blackIncMs = B_INCREMENT;
-  whiteToPlay = true;
-  gameOver = false;
-  clockRunning = false;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(WHITE_BLACK);
-  displayTime();
-#ifdef DEBUG
-  Serial.println("CLOCK_RESET");
-#endif
-}
 
 bool parseTime(String token, uint32_t &outTime, uint32_t &outInc) {
-
   // The string token will be in seconds, we will convert to ms for use with
   // millis
 #ifdef DEBUG
@@ -308,41 +259,52 @@ void processSerialCommand(String cmd) {
   Serial.print(cmd);
   Serial.println("]");
 #endif
-
-  if (cmd == "B") {
-    blackMoved();
-  } else if (cmd == "W") {
-    whiteMoved();
-    // set time for both players
-  } else if (cmd.startsWith("TIME:")) {
-    // Format: TIME:300+5        (both players: 300s + 5s increment)
-    //         TIME:300+5,600+10 (white: 300s+5s, black: 600s+10s)
-    //         TIME:300,600      (white: 300s, black: 600s, no increment)
-    int commaIndex = cmd.indexOf(',');
+  if (cmd.startsWith("TIME:")) {
+    // Format: TIME:300+5;W        (both players: 300s + 5s increment; White to play)
+    //         TIME:300+5,600+10;B (white: 300s+5s, black: 600s+10s;Black to play)
+    //         TIME:300,600;B      (white: 300s, black: 600s, no increment; Black to play)
+    int8_t commaIdx = cmd.indexOf(',');
+    int8_t semiCommaIdx = cmd.indexOf(';');
     uint32_t wTime = 0, wInc = 0, bTime = 0, bInc = 0;
     bool valid = false;
 
-    if (commaIndex > 0) {
+    // figure out who is to play, W or B
+    char toPlay = cmd.charAt(semiCommaIdx + 1);
+
+    if (toPlay == 'W') {
+      whiteToPlay = true;
+      lcd.setCursor(0, 0);
+      lcd.print(WHITE_TURN);
+    }
+    else
+    {
+      whiteToPlay = false;
+      lcd.setCursor(0, 0);
+      lcd.print(BLACK_TURN);
+    }
+
+    if (commaIdx > 0) {
 #ifdef DEBUG
-      Serial.println("commaIndex > 0, [TIME:(black time)+(black inc),(white "
+      Serial.println("commaIdx > 0, [TIME:(black time)+(black inc),(white "
                      "time)+(white inc)] form");
 #endif
       // Two separate tokens — white,black
-      String whiteTime = cmd.substring(5, commaIndex);
+      String whiteTime = cmd.substring(5, commaIdx);
 
 #ifdef DEBUG
       Serial.print("whiteTime token: ");
       Serial.println(whiteTime);
       Serial.print("blackTime token: ");
 #endif
-      String blackTime = cmd.substring(commaIndex + 1);
+      String blackTime = cmd.substring(commaIdx + 1, semiCommaIdx);
+
 #ifdef DEBUG
       Serial.println(blackTime);
 #endif
       valid = parseTime(whiteTime, wTime, wInc) && parseTime(blackTime, bTime, bInc);
     } else {
       // Single token - same time for both players
-      String time = cmd.substring(5);
+      String time = cmd.substring(5, semiCommaIdx);
       valid = parseTime(time, wTime, wInc);
       if (valid) {
         bTime = wTime;
@@ -351,6 +313,7 @@ void processSerialCommand(String cmd) {
     }
 
     if (valid) {
+      clockRunning = true;
       whiteTimeMs = wTime;
       blackTimeMs = bTime;
       whiteIncMs = wInc;
@@ -359,25 +322,22 @@ void processSerialCommand(String cmd) {
       lcd.setCursor(0, 0);
       displayTime();
 #ifdef DEBUG
-      Serial.println("valid token.");
-      Serial.print("TIME_SET:W=");
-      Serial.print(wTime);
-      Serial.print("+");
-      Serial.print(wInc);
-      Serial.print(",B=");
-      Serial.print(bTime);
-      Serial.print("+");
-      Serial.println(bInc);
+      Serial.print("valid token: (");
+      Serial.print(cmd);
+      Serial.print(")");
 #endif
     } else {
       Serial.println("ERROR: Invalid time");
     }
+  } else if (cmd == "START") {
+    reset();
+    clockRunning = true;
   } else if (cmd == "BWON") {
-    blackWon();
+    gameDone();
   } else if (cmd == "WWON") {
-    whiteWon();
+    gameDone();
   } else if (cmd == "DRAW") {
-    draw();
+    gameDone();
   } else if (cmd == "STOP" || cmd == "PAUSE") {
     clockRunning = false;
 
@@ -411,59 +371,6 @@ void processSerialCommand(String cmd) {
 #endif
 }
 
-void setup() {
-
-  Wire.begin(SDA_PIN, SCL_PIN);
-  // start serial connection
-  Serial.begin(9600);
-  while (!Serial) {
-    delay(10);
-  }
-
-  lcd.init();
-  lcd.backlight();
-  reset();
-
-  displayTime();
-  lastUpdate = millis();
-#ifdef DEBUG
-  Serial.println("CLOCK_READY");
-#endif
-  // button pins
-  pinMode(moveBtn.pin, INPUT_PULLUP);
-  pinMode(resetBtn.pin, INPUT_PULLUP);
-}
-
-void checkButton(Button &btn) {
-  int reading = digitalRead(btn.pin);
-
-  // If the reading changed, reset debounce timer
-  if (reading != btn.curRead) {
-    btn.lastDebounceTime = millis();
-    btn.curRead = reading;
-
-    // If enough time has passed, accept the reading as stable
-    if ((millis() - btn.lastDebounceTime) > DEBOUNCE_DELAY) {
-      // If the stable state has changed
-      if (btn.curRead != btn.lastState) {
-        btn.lastState = btn.curRead;
-
-        if (btn.curRead == LOW) {
-
-#ifdef DEBUG
-          Serial.print(btn.name);
-          Serial.println(" pressed");
-#endif
-          if (btn.pin == MOVE_MADE_PIN) {
-            moveMade();
-          } else if (btn.pin == RESET_BTN_PIN) {
-            reset();
-          }
-        }
-      }
-    }
-  }
-}
 
 void loop() {
   // Process serial commands
@@ -492,14 +399,14 @@ void loop() {
         if (whiteTimeMs > 50) {
           whiteTimeMs -= 50;
         } else {
-          whiteTimeOut();
+          gameDone();
           return;
         }
       } else {
         if (blackTimeMs > 50) {
           blackTimeMs -= 50;
         } else {
-          blackTimeOut();
+          gameDone();
           blackTimeMs = 0;
           return;
         }
@@ -511,6 +418,3 @@ void loop() {
   checkButton(moveBtn);
   checkButton(resetBtn);
 }
-
-//  LocalWords:  BWON commaIndex addr greenBtn outTime outInc BWON
-//  LocalWords:  whiteTime blackTime parseTime
